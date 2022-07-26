5a4bab6f9ddfd1c3f87590145fb28e715a1dcdbd
Merijn Vonk <merijnvonk@gmail.com>
2022-07-26 16:17:28 +0200

    added sort buttons

--- src/Components/SongListHeader.js
+++ src/Components/SongListHeader.js
@@ -4,9 +4,21 @@ function SongListHeader ( ) {
     return (
         <div className="song-header">
             <p className="song-header__item">Song</p>
+            <button
+                className="sort-buttons">A-Z</button>
+            <button
+                className="sort-buttons">Z-A</button>
             <p className="song-header__item">Artist</p>
+            <button
+                className="sort-buttons">A-Z</button>
+            <button
+                className="sort-buttons">Z-A</button>
             <p className="song-header__item">Genre</p>
             <p className="song-header__item">Rating</p>
+            <button
+                className="sort-buttons">5-1</button>
+            <button
+                className="sort-buttons">1-5</button>
         </div>
     )
 }
